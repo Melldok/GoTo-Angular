fc946edee4a84dfdcb3b2720e773f9ffc6d3211d
David Dokic <david.dokic28@gmail.com>
2023-10-26 18:32:27 +0200

    Country App Progress

--- 03-countryApp/src/app/app.module.ts
+++ 03-countryApp/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CountriesModule } from './countries/countries.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountriesModule,
+    SharedModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
